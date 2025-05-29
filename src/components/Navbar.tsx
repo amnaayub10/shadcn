2a5885485885ef96f89749a950fd99e7d4ec51e5
Abdul Waheed <ab.waheed1722@gmail.com>
2025-05-29 08:17:49 +0300

    lint errors fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { LogOut, Moon, Settings, Sun, User } from "lucide-react";
-import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import {
   DropdownMenu,
